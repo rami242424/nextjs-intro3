e3cb188e8c024595a1d96bea992ca771fc43c3c1
Rami <138556019+rami242424@users.noreply.github.com>
2024-05-15 22:05:45 +0900

    #3.5 Suspense

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -2,14 +2,12 @@
 
 import { API_URL } from "../app/(home)/page";
 
-async function getVideos(id:string){
-    console.log(`Fetching videos: ${Date.now()}`);  
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+async function getVideos(id: string) {
+    console.log(`Fetching videos: ${Date.now()}`);
+    //await new Promise((resolve) => setTimeout(resolve, 3000));
     const response = await fetch(`${API_URL}/${id}/videos`);
-    return (
-        response.json()  
-    );   
-}
+    return response.json();
+  }
 
 // video 에 관한 ui만 가지기
 export default async function MovieVideos({id}: {id:string}){
