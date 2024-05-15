f493087acb72142369fb72b21fc9eacda321dd34
Rami <138556019+rami242424@users.noreply.github.com>
2024-05-15 20:59:50 +0900

    #3.4 Parallel Requests - 순차적으로 함수가 진행된다

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,6 +1,30 @@
-export default function MovieDetail({params: {id}} : {params: {id: string}}){
-    console.log({params: {id}});
+import { API_URL } from "../../../(home)/page";
+
+// fetch 할 함수(영화 id)
+async function getMovie(id:string) {
+    console.log(`Fetching movies: ${Date.now()}`);  
+    const response = await fetch(`${API_URL}/${id}`);
+    return (
+        response.json()  
+    );                                                            
+}
+
+// fetch 할 함수(영화 video)
+async function getVideos(id:string){
+    console.log(`Fetching videos: ${Date.now()}`);  
+    const response = await fetch(`${API_URL}/${id}/videos`);
+    return (
+        response.json()  
+    );   
+}
+
+export default async function MovieDetail({params: {id}} : {params: {id: string}}){
+    // console.log({params: {id}});
+    console.log('start fetching');
+    const movie = await getMovie(id);
+    const video = await getVideos(id);
+    console.log('end fetching');
     return(
-        <h1>Movie detail {id}</h1>
+        <h1>{movie.title}</h1>
     );
 }
