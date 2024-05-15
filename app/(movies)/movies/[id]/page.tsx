87657852281ca59766d0f9912bd8540afd877b47
Rami <138556019+rami242424@users.noreply.github.com>
2024-05-15 21:55:51 +0900

    #3.5 Suspense - suspense 컴포넌트 안에서 동시다발적으로 함수가 실행되고, 완료되는 대로 화면에 즉각적으로 보여짐

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,33 +1,16 @@
-import { API_URL } from "../../../(home)/page";
-
-// fetch 할 함수(영화 id)
-async function getMovie(id:string) {
-    console.log(`Fetching movies: ${Date.now()}`);  
-    await new Promise((resolve) => setTimeout(resolve, 5000));
-    const response = await fetch(`${API_URL}/${id}`);
-    return (
-        response.json()  
-    );                                                            
-}
-
-// fetch 할 함수(영화 video)
-async function getVideos(id:string){
-    console.log(`Fetching videos: ${Date.now()}`);  
-    await new Promise((resolve) => setTimeout(resolve, 5000));
-    const response = await fetch(`${API_URL}/${id}/videos`);
-    return (
-        response.json()  
-    );   
-}
+import { Suspense } from "react";
+import MovieInfo from "../../../../components/movie-info";
+import MovieVideos from "../../../../components/movie-videos";
 
 export default async function MovieDetail({params: {id}} : {params: {id: string}}){
-    console.log('===============');
-    console.log('start fetching');
-    // Promise.all을 사용함으로써 하나씩 끝내고 그 결과값을 movie, videos에 순차적으로 넘겨준다
-    // Promise.all([getMovie(id), getVideos(id)]);
-    const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
-    console.log('end fetching');
     return(
-        <h1>{movie.title}</h1>
+        <div>
+            <Suspense fallback={<h1>Loading movie info</h1>}>
+                <MovieInfo id={id} />
+            </Suspense>
+            <Suspense fallback={<h1>Loading movie videos</h1>}>
+                <MovieVideos id={id} />
+            </Suspense>
+        </div>
     );
 }
