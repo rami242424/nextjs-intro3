9097b3252a60424d8758a9c786a36a0fa74315be
Rami <138556019+rami242424@users.noreply.github.com>
2024-05-15 17:45:24 +0900

    #3.1 Client Side - normal react 앱에서 (외부 라이브러리나 패키지 없이)fetch하는 방법

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,12 +1,27 @@
+"use client";
+import { useEffect, useState } from "react";
+
 // http://localhost:3000
-export const metadata = {
-    title: 'Home',
-    }
+// export const metadata = {
+//     title: 'Home',
+//     } // => client component에서는 metadata를 export할 수 없다.
 
 export default function App(){
+    const [isLoading, setIsLoading] = useState(true);
+    const [movies, setMovies] = useState([]);
+    const getMovies = async () => {
+        const response = await fetch(`https://nomad-movies.nomadcoders.workers.dev/movies`);
+        const json = await response.json();
+        setMovies(json);
+        setIsLoading(false);
+    }
+    useEffect(()=> {
+        getMovies();
+    }, []);
+
     return (
         <div>
-            <h1>Hello App</h1>
+            {isLoading ? "Loading.." : JSON.stringify(movies)}
         </div>
     );
 }
