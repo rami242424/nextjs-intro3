1019fdcaaffda8ce89467df4cf68a5cc9cb963df
Rami <138556019+rami242424@users.noreply.github.com>
2024-05-16 18:22:36 +0900

    #4.3 Movie Trailers

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,5 +1,6 @@
 // server component
 import { API_URL } from "../app/(home)/page";
+import styles from "../styles/movie-info.module.css";
 
 async function getMovie(id:string) {
     console.log(`Fetching movies: ${Date.now()}`);  
@@ -14,6 +15,16 @@ async function getMovie(id:string) {
 export default async function MovieInfo({id}: {id:string}){
     const movie = await getMovie(id);
     return (
-        <h6>{JSON.stringify(movie)}</h6>
+        <div className={styles.container}>
+            <img src={movie.poster_path} className={styles.poster} />
+            <div className={styles.info}>
+                <h3>Release Date : {movie.release_date}</h3>
+                <h1 className={styles.title}>{movie.title}</h1>
+                <h3>⭐️{movie.vote_average.toFixed(0)}</h3>
+                <h4>Company : {movie.production_companies[0].name} [{movie.production_companies[0].origin_country}]</h4>
+                <p>{movie.overview}</p>
+                <a href={movie.homepage} target={"_blank"}>Home &rarr;</a>
+            </div>
+        </div>
     );
 }
--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,19 +1,27 @@
 // server component
-
+import styles from "../styles/movie-videos.module.css";
 import { API_URL } from "../app/(home)/page";
 
 async function getVideos(id: string) {
-    console.log(`Fetching videos: ${Date.now()}`);
-    await new Promise((resolve) => setTimeout(resolve, 3000));
-    throw new Error('something broken');
-    // const response = await fetch(`${API_URL}/${id}/videos`);
-    // return response.json();
+    // console.log(`Fetching videos: ${Date.now()}`);
+    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    // throw new Error('something broken');
+    const response = await fetch(`${API_URL}/${id}/videos`);
+    return response.json();
   }
 
 // video 에 관한 ui만 가지기
 export default async function MovieVideos({id}: {id:string}){
     const videos = await getVideos(id);
     return (
-        <h6>{JSON.stringify(videos)}</h6>
+        <div className={styles.container}>
+            {videos.map((video) => (
+                <iframe 
+                    key={video.id} 
+                    src={`https://youtube.com/embed/${video.key}`} 
+                    title={video.title}
+                />
+            ))}
+        </div>
     );
 }
